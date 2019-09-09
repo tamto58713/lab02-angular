26ab8b5c88a874d04964891b431e19a546cdc68c
tamtotran5667 <amire.nikolatesla@gmail.com>
2019-09-10 04:32:17 +0700

    add route for app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './students/students.component'
+import { DetailStudentComponent} from './detail-student/detail-student.component'
+import { WrongPathComponent } from './wrong-path/wrong-path.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StudentsComponent},
+  { path: 'student/:id', component: DetailStudentComponent},
+  { path: "**", component: WrongPathComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/detail-student/detail-student.component.ts
+++ src/app/detail-student/detail-student.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; 
+import {Location} from '@angular/common'; 
 import { students } from '../students'
 import { courses } from '../courses'
 import { enrollments } from '../enrollments'
@@ -9,28 +11,37 @@ import { enrollments } from '../enrollments'
 })
 export class DetailStudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
   student
   courses = {}
   enrollments
-  id = 1;
   listStudents = [...students]
   listCourses = [...courses]
   listEnrollments = [...enrollments]
   ngOnInit() {
-    this.student = this.listStudents.filter(student => {
-      return student.id === this.id
-    })[0]
+    this.route.paramMap.subscribe(params => {
+      let id = parseInt(params.get('id'))
+      if (!id) {
+        this.location.replaceState("/student/1")
+        id = 1
+      }
 
-    this.enrollments = this.listEnrollments.filter(enrollment => {
-      return enrollment.studentId === this.id
+      this.student = this.listStudents.filter(student => {
+        return student.id === id
+      })[0]
+  
+      this.enrollments = this.listEnrollments.filter(enrollment => {
+        return enrollment.studentId === id
+      })
+  
+      for (let i = 0; i < enrollments.length; i++) {
+        this.courses[enrollments[i].courseId] = this.listCourses.filter(course => {
+          return course.courseId === enrollments[i].courseId
+        })[0]
+      }
     })
 
-    for (let i = 0; i < enrollments.length; i++) {
-      this.courses[enrollments[i].courseId] = this.listCourses.filter(course => {
-        return course.courseId === enrollments[i].courseId
-      })[0]
-    }
+    
 
   }
 }
