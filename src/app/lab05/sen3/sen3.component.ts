1a8ae4c855a3cf192385367bd93873094f077e0b
tamto58713 <tamtotran5667@gmail.com>
2019-10-06 23:43:26 +0700

    Completed exercise 3 lab05

--- src/app/lab05/sen3/sen3.component.ts
+++ src/app/lab05/sen3/sen3.component.ts
@@ -9,7 +9,52 @@ export class Sen3Component implements OnInit {
 
   constructor() { }
 
+
+  checked = {}
+
+  quantity = {}
+
+  total = 0
   ngOnInit() {
+    this.products.map(product => {
+      this.checked[product.id] = false
+      this.quantity[product.id] = 0
+    })
   }
+
+  onChange(id) {
+    console.log(parseInt(this.quantity[id]))
+    if (!parseInt(this.quantity[id]))
+      this.quantity[id] = 0
+    else
+      this.quantity[id] = parseInt(this.quantity[id])
+
+    this.total = this.products.reduce((a, b) => {
+      return a + b.price * this.quantity[b.id]
+    }, 0)
+  }
+  products = [
+    {
+      id: 1,
+      name: "Soft Drink",
+      price: 10000
+    },
+    {
+      id: 2,
+      name: "Black Coffee",
+      price: 20000
+    },
+    {
+      id: 3,
+      name: "Coca Cola",
+      price: 12000
+    },
+    {
+      id: 4,
+      name: "Number One",
+      price: 13000
+    }
+  ]
+
 
 }
