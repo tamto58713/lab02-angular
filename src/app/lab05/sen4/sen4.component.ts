877401a56616911e48ba2567f74c17038512f257
tamto58713 <tamtotran5667@gmail.com>
2019-10-07 00:07:35 +0700

    completed exercise 4 lab05

--- src/app/lab05/sen4/sen4.component.ts
+++ src/app/lab05/sen4/sen4.component.ts
@@ -9,7 +9,36 @@ export class Sen4Component implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  isSalary
+  name
+  salary
+  gender
+  groupAge
+
+  onChangeName() {
+    this.name = this.name.trim()
   }
+
+  onChangeGender(gender) {
+    this.gender = gender
+  }
+
+  onChangeSalary() {
+    if (parseInt(this.salary).toString() !== this.salary.toString()) {
+      this.isSalary = false
+      this.salary = ""
+  }
+    else {
+      this.isSalary = true
+      if (this.salary)
+        this.salary = parseInt(this.salary)
+      else
+        this.salary = ""
+    }
+  }
+  ngOnInit() {
+    this.salary = ""
+  }
+
 
 }
